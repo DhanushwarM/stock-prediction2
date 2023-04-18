11a3a03fd62ac700a23aaaa386aca1aae4c10f70
DhanushwarM <116282528+DhanushwarM@users.noreply.github.com>
2023-04-18 18:56:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 # import matplotlib.pyplot as plt
-import yfinance as yf # https://pypi.org/project/yfinance/
+# import yfinance as yf # https://pypi.org/project/yfinance/
 
 ##############################
 # Technical Analysis Classes #
